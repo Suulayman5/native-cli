605b417cb14f70ef498325f406acd2495a7ab2e5
suulayman5 <ajahsuleiman@gmail.com>
2025-02-09 02:41:36 +0100

    Add search input and category icons to HomeScreen component

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
-import { fontSize, spacing } from '../constants/dimention'
+import { fontSize, iconSize, spacing } from '../constants/dimention'
 import { colors } from '../constants/colors'
 import { fontFamily } from '../constants/fonts'
 
@@ -8,6 +8,15 @@ const HomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.headline}>Find Your Suitable Watch Now</Text>
+      <View style={styles.mainInputContainer}>
+        <View style={styles.inputWrapper}>
+          <Image source={require('../assets/search.png')} style={styles.logo} />
+          <TextInput style={styles.textInput} placeholder="Search Product" />
+        </View>
+        <View style={styles.categoryContainer}>
+          <Image source={require('../assets/category.png')} style={styles.logo} />
+        </View>
+      </View>
     </View>
   )
 }
@@ -25,8 +34,31 @@ const styles = StyleSheet.create({
         fontFamily: fontFamily.Bold,
         color: colors.black,
     },
-    text: {
-        color: colors.gray,
-        fontSize: 20
-    }
+    mainInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: spacing.xl,
+    },
+    inputWrapper: {
+        flex: 1,
+        borderWidth: 1,
+        flexDirection: 'row',
+        alignItems:'center',
+        borderColor:colors.placeholderText,
+        borderRadius: 44,
+        paddingHorizontal: spacing.md,
+    },
+    logo: {
+        width: iconSize.md,
+        height: iconSize.md,
+    },
+    textInput: {
+        flex: 1,
+        paddingHorizontal: spacing.sm,
+        fontSize: fontSize.md,
+    },
+    categoryContainer: {
+        paddingHorizontal: spacing.sm,
+    },
+
 })
